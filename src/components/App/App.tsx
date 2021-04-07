4ced7cd59ea50dc2ec5951525e6213a3a9302eef
Aleksey Petrov <hydrognomik@gmail.com>
2021-04-07 11:09:58 +0300

    Initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import { h } from "preact";
+
+import styles from "./styles.module.scss";
+
+export const App = () => {
+  return <h1 className={styles.heading}>Hello, world!</h1>;
+};
---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import { render, h } from "preact";
+import { App } from "~/src/components/App/App";
+
+render(<App />, document.getElementById("root"));
